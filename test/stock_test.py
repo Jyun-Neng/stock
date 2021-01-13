6685666ef10fe4d7ef71bbeb26e1dfc46beea7ca
Ji Jyun-Neng <jyunnengji@gmail.com>
2021-01-14 00:22:39 +0800

    Update unit test.
    - Add fetchn function test.
    - Remove getDate and getDailyPricing tests.

--- test/stock_test.py
+++ test/stock_test.py
@@ -5,20 +5,24 @@ from stock import stock
 
 
 class StockTest(unittest.TestCase):
+
     def setUp(self):
-        self.args = argparse.Namespace(id="2885",
-                                       year=2021,
-                                       month=1,
-                                       url="127.0.0.1",
-                                       port=27017)
+        self.args = argparse.Namespace(
+            id="2885", year=2021, month=1, url="127.0.0.1", port=27017)
         self.year = self.args.year
         self.month = self.args.month
         self.stock = stock.Stock(self.args)
 
     def test_fetch(self):
-        stock_info_list = self.stock.fetch(self.year, self.month)
+        stock_info_list = self.stock.fetch(2021, 1)
         self.assertTrue(stock_info_list is not None,
                         "Stock info is not fetched")
+
+    def test_fetchn(self):
+        stock_info_list = self.stock.fetchn(2020, 1, 13)
+        date = stock_info_list[-1].date
+        self.assertTrue(date.year == 2021 and date.month == 1,
+                        "Fetched date is wrong")
 
     def test_fetchFrom(self):
         today = datetime.today()
@@ -28,16 +32,6 @@ class StockTest(unittest.TestCase):
         for daily_info in self.stock.daily_info_list:
             self.assertTrue(today >= daily_info.date >= start_date,
                             "Fetch date is not in specified date")
-
-    def test_getDate(self):
-        self.stock.fetch(self.year, self.month)
-        self.assertEqual(self.stock.getDate[0], datetime(2020, 1, 2),
-                         "Get wrong date")
-
-    def test_getDailyPricing(self):
-        self.stock.fetch(self.year, self.month)
-        self.assertEqual(self.stock.getDailyPricing[0], 0.0,
-                         "Get wrong daily pricing")
 
 
 if __name__ == "__main__":
